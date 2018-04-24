4c1e42614bd9c53524b13090dbf8319013f95c18
Buanaroti <belickyianton@mail.ru>
2018-04-24 09:16:21 +0300

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	//slider
+	$('.js-main-slider').slick({
+		dots: true, 
+		arrows: false, 
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		autoplay: true
+	});
+
+	//scroll-link
+	$('.js-scroll-link').on('click', function(e) {
+		e.preventDefault();
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: ($(anchor.attr('href')).offset().top)
+		}, 700);
+	});
+});
