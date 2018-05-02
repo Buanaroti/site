4dc0ad1c63e8c579e048d6fd60ace72c8fcc5dd3
Buanaroti <belickyianton@mail.ru>
2018-05-02 13:07:37 +0300

    partners

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 	//slider
-	$('.js-main-slider').slick({
+	$('.js-slider-otziv').slick({
 		dots: false, 
 		arrows: false, 
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		fade: true,
 		autoplay: true,
-		autoplaySpeed: 1000
+		autoplaySpeed: 2000
+	});
+	$('.js-slider-klient').slick({
+		lazyLoad: 'ondemand',
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		arrows: false,
 	});
 
 	//scroll-link
